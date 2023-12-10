211d61f64371438b0fbe488a47fd37e4e8b4fe4d
Travis Furrer <39924916+tcfurrer@users.noreply.github.com>
2023-12-09 16:00:50 -0800

    Initial coding for button lists, etc.

--- app/src/main/java/app/AppController.java
+++ app/src/main/java/app/AppController.java
@@ -3,27 +3,59 @@ package app;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import static javafx.scene.paint.Color.BLACK;
 
 public final class AppController
 {
-
-    @FXML private Label xLabel, yLabel;
+    @FXML private Spinner<Integer> stepSize;
     @FXML private Canvas canvas;
+    @FXML private VBox vBox;
+    @FXML private HBox hBox;
 
+    private final ToggleButtonList xButtons, yButtons;
     private GraphicsContext gc;
+
+    public AppController()
+    {
+        xButtons = new ToggleButtonList();
+        yButtons = new ToggleButtonList();
+    }
 
     @FXML
     void initialize()
     {
+        // Step-size spinner
+        //
+        var valueFactory = new IntegerSpinnerValueFactory(10,100,30, 1);
+        stepSize.setValueFactory(valueFactory);
+
+        // Buttons
+        //
+        xButtons.parentProperty().set(hBox);
+        yButtons.parentProperty().set(vBox);
+        xButtons.stepSizeProperty().bind(stepSize.valueProperty());
+        yButtons.stepSizeProperty().bind(stepSize.valueProperty());
+
+        // Canvas
+        //
         gc = canvas.getGraphicsContext2D();
-        resetCanvas();
+        draw();
+        xButtons.buttons().subscribe(this::draw);
+        yButtons.buttons().subscribe(this::draw);
     }
 
-    private void resetCanvas()
+    private void draw()
     {
+        // Clear entire canvas to background color
+        //
         gc.setFill(BLACK);
         gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
+
+        // Draw the pattern
+        //
     }
 }
---
+++ app/src/main/java/app/ToggleButtonList.java
@@ -0,0 +1,95 @@
+package app;
+
+import javafx.beans.Observable;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.util.Subscription;
+import javafx.util.Callback;
+import java.util.*;
+
+final class ToggleButtonList
+{
+    private final ObjectProperty<Pane> parent;
+    private final IntegerProperty stepSize;
+    private final ReadOnlyListWrapper<ToggleButton> buttons;
+
+    private Subscription parentSizeSubscription = Subscription.EMPTY;
+
+    ToggleButtonList()
+    {
+        parent = new SimpleObjectProperty<>();
+        stepSize = new SimpleIntegerProperty(10);
+        Callback<ToggleButton, Observable[]> extractor =
+                button -> List.of(button.selectedProperty()).toArray(new Observable[]{});
+        buttons = new ReadOnlyListWrapper<>(FXCollections.observableArrayList(extractor));
+
+        parent.subscribe((oldParent,newParent) -> {
+            if (oldParent != null)
+            {
+                removeButtons(oldParent);
+            }
+            if (newParent != null)
+            {
+                addButtons(newParent);
+            }
+        });
+    }
+
+    public ObjectProperty<Pane> parentProperty() { return parent; }
+    public IntegerProperty stepSizeProperty() { return stepSize; }
+    public ReadOnlyListProperty<ToggleButton> buttons() { return buttons.getReadOnlyProperty(); }
+
+    private void updateButtons()
+    {
+        var pane = parent.get();
+        removeButtons(pane);
+        addButtons(pane);
+    }
+    private void addButtons(Pane pane)
+    {
+        //
+        // Compute the number of buttons needed to fill the given pane
+        //
+        int paneSize = switch (pane)
+        {
+            case VBox vBox -> (int)Math.floor(vBox.getHeight());
+            case HBox hBox -> (int)Math.floor(hBox.getWidth());
+            default -> throw new UnsupportedOperationException();
+        };
+        int numButtons = paneSize / stepSize.get();
+
+        //
+        // Create the buttons and add them to the pane
+        //
+        buttons.clear();
+        for (int i=0; i<numButtons; i++)
+        {
+            var button = new ToggleButton();
+            buttons.add(button);
+        }
+        pane.getChildren().addAll(buttons);
+
+        //
+        // Ensure that buttons always fill up the parent pane
+        //
+        parentSizeSubscription = switch (pane)
+        {
+            case VBox vBox -> vBox.heightProperty().subscribe(this::updateButtons);
+            case HBox hBox -> hBox.widthProperty().subscribe(this::updateButtons);
+            default -> throw new UnsupportedOperationException();
+        };
+
+    }
+
+    private void removeButtons(Pane pane)
+    {
+        parentSizeSubscription.unsubscribe();
+        parentSizeSubscription = Subscription.EMPTY;
+        pane.getChildren().removeAll(buttons);
+        buttons.clear();
+    }
+}
